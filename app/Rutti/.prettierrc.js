24c0d1a49299a4c3f68a71e21682deaab8bd923a
Oscar Andersson <paroscarandersson@live.com>
2020-02-17 16:09:53 +0100

    eslint and prettier

--- app/Rutti/.prettierrc.js
+++ app/Rutti/.prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
 };
--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,71 +1,71 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-const express = require("express");
-const axios = require("axios");
-var cors = require("cors");
+const express = require('express');
+const axios = require('axios');
+var cors = require('cors');
 
 const app = express();
 app.use(cors());
 
-app.get("")
+app.get('');
 
-app.post("/:productSku", (req, res) => {
-  const { productSku } = req.params;
+app.post('/:productSku', (req, res) => {
+    const { productSku } = req.params;
 
-  if (!/^-?\d+\.?\d*$/.test(productSku)) {
-    return res.json({ error: "Faulty SKU" });
-  }
-
-  const { stores } = JSON.parse(req.body);
-
-  if (!stores || stores.length < 1) {
-    return res.json({ error: "No stores." });
-  }
-
-  let requestMap = new Map();
-  let requests = [];
-
-  function addRequest(url, store) {
-    requestMap.set(url, store);
-    requests.push(axios.get(url));
-  }
-
-  stores.forEach(store => {
-    if (store.retailer === "ica") {
-      console.log("retailer ica");
-      addRequest(
-        `https://handla.ica.se/api/content/v1/collections/customer-type/B2C/store/${store.storeId}/products?productIds=${productSku}`,
-        store
-      );
-    } else if (store.retailer === "coop") {
-      console.log("retailer coop");
-      addRequest(
-        `https://www.coop.se/ws/v2/coop/users/anonymous/products/${productSku}?fields=FULL&storeId=${store.storeId}`,
-        store
-      );
-    } else if (store.retailer === "citygross") {
-      console.log("retailer citygross");
-      addRequest(
-        `https://www.citygross.se/api/v1/esales/search/?Q=${productSku}&page=0&store=${store.storeId}`,
-        store
-      );
+    if (!/^-?\d+\.?\d*$/.test(productSku)) {
+        return res.json({ error: 'Faulty SKU' });
     }
-  });
 
-  axios
-    .all(requests)
-    .then(results => {
-      let productResult = [];
+    const { stores } = JSON.parse(req.body);
 
-      results.map(result =>
-        productResult.push({
-          store: requestMap.get(result.config.url),
-          data: result.data
+    if (!stores || stores.length < 1) {
+        return res.json({ error: 'No stores.' });
+    }
+
+    let requestMap = new Map();
+    let requests = [];
+
+    function addRequest(url, store) {
+        requestMap.set(url, store);
+        requests.push(axios.get(url));
+    }
+
+    stores.forEach(store => {
+        if (store.retailer === 'ica') {
+            console.log('retailer ica');
+            addRequest(
+                `https://handla.ica.se/api/content/v1/collections/customer-type/B2C/store/${store.storeId}/products?productIds=${productSku}`,
+                store
+            );
+        } else if (store.retailer === 'coop') {
+            console.log('retailer coop');
+            addRequest(
+                `https://www.coop.se/ws/v2/coop/users/anonymous/products/${productSku}?fields=FULL&storeId=${store.storeId}`,
+                store
+            );
+        } else if (store.retailer === 'citygross') {
+            console.log('retailer citygross');
+            addRequest(
+                `https://www.citygross.se/api/v1/esales/search/?Q=${productSku}&page=0&store=${store.storeId}`,
+                store
+            );
+        }
+    });
+
+    axios
+        .all(requests)
+        .then(results => {
+            let productResult = [];
+
+            results.map(result =>
+                productResult.push({
+                    store: requestMap.get(result.config.url),
+                    data: result.data
+                })
+            );
+            return res.json(productResult);
         })
-      );
-      return res.json(productResult);
-    })
-    .catch(error => res.json(error));
+        .catch(error => res.json(error));
 });
 
-exports.api = functions.region("europe-west2").https.onRequest(app);
+exports.api = functions.region('europe-west2').https.onRequest(app);
