8b7898f9d14a5df0ea328dd5c983b8d7cb5edc5a
Oscar Andersson <paroscarandersson@live.com>
2020-02-18 09:29:32 +0100

    add citygross

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -2,24 +2,91 @@ const functions = require('firebase-functions');
 
 const express = require('express');
 const axios = require('axios');
+const getNearbyCityGrossStores = require('./citygross');
 var cors = require('cors');
 
 const app = express();
 app.use(cors());
 
-app.get('');
+// app.get('/citygross', async (req, res) => {
+//     if (req.method !== 'GET') {
+//         res.status(405).json({ error: 'Request method not allowed.' });
+//     }
+//     let { zipCode } = req.query;
+
+//     if (!zipCode) {
+//         res.status(400).json({ error: 'No zipCode parameter.' });
+//     }
+
+//     const nearbyStores = await getNearbyCityGrossStores(zipCode);
+
+//     res.json(nearbyStores);
+// });
+
+app.get('/findStores', (req, res) => {
+    if (req.method !== 'GET') {
+        res.status(405).json({ error: 'Request method not allowed.' });
+    }
+    const { zipCode } = req.query;
+
+    if (!zipCode) {
+        res.status(400).json({ error: 'No zipCode parameter.' });
+    }
+
+    let requestMap = new Map();
+    let requests = [];
+
+    function addRequest(url, store) {
+        requestMap.set(url, store);
+        requests.push(axios.get(url));
+    }
+
+    addRequest(
+        `https://handla.ica.se/api/store/v1?zip=${zipCode}&customertype=B2C`,
+        'ICA',
+    );
+
+    addRequest(
+        `https://www.coop.se/ws/v2/coop/pointofservices?query=${zipCode}`,
+        'COOP',
+    );
+
+    requests.push(getNearbyCityGrossStores(zipCode));
+
+    axios
+        .all(requests)
+        .then(results => {
+            let storesResults = [];
+
+            results.map(result =>
+                storesResults.push({
+                    store: result.config
+                        ? requestMap.get(result.config.url)
+                        : 'CITYGROSS',
+                    data: result.config ? result.data : result,
+                }),
+            );
+
+            return res.status(200).json(storesResults);
+        })
+        .catch(error => res.status(500).json(error));
+});
 
 app.post('/:productSku', (req, res) => {
+    if (req.method !== 'POST') {
+        res.status(405).json({ error: 'Request method not allowed.' });
+    }
+
     const { productSku } = req.params;
 
     if (!/^-?\d+\.?\d*$/.test(productSku)) {
-        return res.json({ error: 'Faulty SKU' });
+        return res.status(400).json({ error: 'Faulty SKU' });
     }
 
     const { stores } = JSON.parse(req.body);
 
     if (!stores || stores.length < 1) {
-        return res.json({ error: 'No stores.' });
+        return res.status(400).json({ error: 'No stores.' });
     }
 
     let requestMap = new Map();
@@ -35,19 +102,19 @@ app.post('/:productSku', (req, res) => {
             console.log('retailer ica');
             addRequest(
                 `https://handla.ica.se/api/content/v1/collections/customer-type/B2C/store/${store.storeId}/products?productIds=${productSku}`,
-                store
+                store,
             );
         } else if (store.retailer === 'coop') {
             console.log('retailer coop');
             addRequest(
                 `https://www.coop.se/ws/v2/coop/users/anonymous/products/${productSku}?fields=FULL&storeId=${store.storeId}`,
-                store
+                store,
             );
         } else if (store.retailer === 'citygross') {
             console.log('retailer citygross');
             addRequest(
                 `https://www.citygross.se/api/v1/esales/search/?Q=${productSku}&page=0&store=${store.storeId}`,
-                store
+                store,
             );
         }
     });
@@ -60,12 +127,12 @@ app.post('/:productSku', (req, res) => {
             results.map(result =>
                 productResult.push({
                     store: requestMap.get(result.config.url),
-                    data: result.data
-                })
+                    data: result.data,
+                }),
             );
-            return res.json(productResult);
+            return res.status(200).json(productResult);
         })
-        .catch(error => res.json(error));
+        .catch(error => res.status(500).json(error));
 });
 
 exports.api = functions.region('europe-west2').https.onRequest(app);
