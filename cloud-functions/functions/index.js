b227b1d6c4ad1d8a2104e8322d13b4175917be4a
Oscar Andersson <paroscarandersson@live.com>
2020-02-24 16:31:51 +0100

    cloud functions for it all

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,5 +1,4 @@
 const functions = require('firebase-functions');
-
 const express = require('express');
 const axios = require('axios');
 const getNearbyCityGrossStores = require('./citygross');
@@ -72,7 +71,31 @@ app.get('/findStores', (req, res) => {
         .catch(error => res.status(500).json(error));
 });
 
-app.post('/:productSku', (req, res) => {
+app.post('/getProducts', async (req, res) => {
+    if (req.method !== 'POST') {
+        res.status(405).json({ error: 'Request method not allowed.' });
+    }
+
+    const { stores, productSkus } = req.body;
+
+    if (!stores || stores.length < 1) {
+        return res.status(400).json({ error: 'No stores.' });
+    }
+
+    let requests = [];
+
+    productSkus.forEach(productSku => {
+        if (/^-?\d+\.?\d*$/.test(productSku)) {
+            requests.push(getProductInfo(productSku, stores));
+        }
+    });
+
+    const results = await axios.all(requests);
+
+    res.json(results);
+});
+
+app.post('/getProduct/:productSku', async (req, res) => {
     if (req.method !== 'POST') {
         res.status(405).json({ error: 'Request method not allowed.' });
     }
@@ -89,50 +112,67 @@ app.post('/:productSku', (req, res) => {
         return res.status(400).json({ error: 'No stores.' });
     }
 
-    let requestMap = new Map();
-    let requests = [];
-
-    function addRequest(url, store) {
-        requestMap.set(url, store);
-        requests.push(axios.get(url));
-    }
-
-    stores.forEach(store => {
-        if (store.retailer === 'ica') {
-            console.log('retailer ica');
-            addRequest(
-                `https://handla.ica.se/api/content/v1/collections/customer-type/B2C/store/${store.storeId}/products?productIds=${productSku}`,
-                store,
-            );
-        } else if (store.retailer === 'coop') {
-            console.log('retailer coop');
-            addRequest(
-                `https://www.coop.se/ws/v2/coop/users/anonymous/products/${productSku}?fields=FULL&storeId=${store.storeId}`,
-                store,
-            );
-        } else if (store.retailer === 'citygross') {
-            console.log('retailer citygross');
-            addRequest(
-                `https://www.citygross.se/api/v1/esales/search/?Q=${productSku}&page=0&store=${store.storeId}`,
-                store,
-            );
-        }
-    });
-
-    axios
-        .all(requests)
-        .then(results => {
-            let productResult = [];
-
-            results.map(result =>
-                productResult.push({
-                    store: requestMap.get(result.config.url),
-                    data: result.data,
-                }),
-            );
-            return res.status(200).json(productResult);
-        })
-        .catch(error => res.status(500).json(error));
+    res.json(await getProductInfo());
 });
+
+async function getProductInfo(productSku, stores) {
+    try {
+        let requestMap = new Map();
+        let requests = [];
+
+        function addRequest(url, store) {
+            requestMap.set(url, store);
+            requests.push(axios.get(url).catch(exception => null));
+        }
+
+        stores.forEach(store => {
+            if (store.retailer === 'ica') {
+                addRequest(
+                    `https://handla.ica.se/api/content/v1/collections/customer-type/B2C/store/${store.storeId}/products?productIds=${productSku}`,
+                    store,
+                );
+            } else if (store.retailer === 'coop') {
+                addRequest(
+                    `https://www.coop.se/ws/v2/coop/users/anonymous/products/${productSku}?fields=FULL&storeId=${store.storeId}`,
+                    store,
+                );
+            } else if (store.retailer === 'citygross') {
+                addRequest(
+                    `https://www.citygross.se/api/v1/esales/search/?Q=${productSku}&page=0&store=${store.storeId}`,
+                    store,
+                );
+            }
+        });
+
+        const results = await axios.all(requests);
+
+        let productResult = [];
+
+        results.map(result => {
+            if (result) {
+                const store = requestMap.get(result.config.url);
+                let data = {};
+                switch (store.retailer) {
+                    case 'ica':
+                        data = result.data.length ? result.data : null;
+                        break;
+                    case 'coop':
+                        data = result.data;
+                        break;
+                    case 'citygross':
+                        data = result.data.data[0] ? result.data.data[0] : null;
+                        break;
+                }
+                if (data) {
+                    productResult.push({ store: store, data: data });
+                }
+            }
+        });
+
+        return productResult;
+    } catch (exception) {
+        return exception;
+    }
+}
 
 exports.api = functions.region('europe-west2').https.onRequest(app);
